437ec909a2fa57675fbc59b12e5a73345a9c93a2
Nethelbert <gilblasse@gmail.com>
2020-01-19 12:32:57 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,17 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active 
+      @student.active = false
+    else 
+      @student.active = true
+    end
+     @student.save
+     redirect_back fallback_location: { action: "show", id: @student.id }
+  end
 
   private
 
