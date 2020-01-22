90cd07d5a6680aeea86b556b3fe63bc574351db6
LucyWeltner <lucyiweltner@gmail.com>
2020-01-22 16:24:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	get '/students' => 'students#index'
+	get '/students/:id' => 'students#show', as: "student"
+	get 'students/:id/activate' => 'students#activate', as: "activate_student"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
