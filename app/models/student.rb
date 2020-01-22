60d3a8850bbbdf108556814762e59c15271eb098
C-Mike576 <m.calkins576@gmail.com>
2020-01-22 15:59:53 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,20 @@
 class Student < ActiveRecord::Base
+  after_initialize :init
+  
+  def init
+    self.active ||= false 
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def active?
+    if self.active
+      "active"
+    else
+      "inactive"
+    end
+  end
+
 end
