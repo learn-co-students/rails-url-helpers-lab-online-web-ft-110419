4710e6fd3e2d6c3ce18d0f73a83939a3afa7b0c2
Ivana <ivana_pile@yahoo.com>
2020-03-20 23:44:37 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,22 @@
 class Student < ActiveRecord::Base
-  def to_s
-    self.first_name + " " + self.last_name
+ 
+  def to_s	  
+    self.first_name + " " + self.last_name	    
+  end	  
+
+  def check_active
+    if self.active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
   end
-  def is_active
-   @student.active == true
+
+  def change_active
+    if self.active
+      self.active = false
+    else
+      self.active = true
+    end
   end
-end
+end 	
